454a1efa2c46ce4dc0264d8ed5188858a349b093
Owen Yamauchi <owen.yamauchi@gmail.com>
2023-07-03 18:27:13 -0400

    Reset omnibox keyword, remove unnecessary stuff

--- js/contentscript.js
+++
@@ -1,8 +0,0 @@
-// Remind users of Wikipedia Search on Wikipedia home page
-if (window.location.href.includes("www.wikipedia.org") && document.body.querySelector(".search-container")) {
-	var searchContainer = document.body.querySelector(".search-container")
-	// Create message
-	var message = document.createElement('div')
-	message.innerText = "You can search Wikipedia from your browser's search bar. Just type 'wiki' in the search bar, then press the Tab key, then type the name of the article."
-	searchContainer.appendChild(message)
-}
